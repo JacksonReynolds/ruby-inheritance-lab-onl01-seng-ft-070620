3824bb75fe5ba62dd9fd8a2604d5d6c4176e2cfc
Jackson Reynolds <jwreynold@gmail.com>
2020-07-21 16:26:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
 class Student < User
 
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end #initialize
+
+  def learn(some_knowledge)
+    self.knowledge << some_knowledge
+  end #learn
+
 end
